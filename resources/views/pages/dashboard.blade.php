dbd26d01a34f5c31ef70d681eb0a583f7153a21b
AIIA\fabian <fabian@aiia.co.id>
2023-07-12 13:58:52 +0700

    dev-1.0.0: mqtt config

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -88,7 +88,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"
     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mqtt/4.2.7/mqtt.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <script>
     // chart option
@@ -180,47 +179,52 @@
             updateChart(item.line, item.items);
         });
     });
+    Paho.MQTT.DEBUG = true;
 
     let client;
 
     function connectMQTT() {
         // Create an MQTT client instance
-        const clientId = "client_" + Math.random().toString(16).substr(2, 8);
-        client = mqtt.connect('mqtt://172.18.3.70:1883', {
-            clientId: clientId,
-            // username: 'fabian',
-            // password: '1234'
-        });
+        clientId = "client_" + Math.random().toString(16).substr(2, 8);
+        client = new Paho.MQTT.Client("172.18.3.70", Number(1883), clientId);
 
         // Set callback handlers
-        client.on('connect', onConnect);
-        client.on('message', onMessageArrived);
-        client.on('close', onConnectionLost);
-        client.on('error', onFailure);
+        client.onConnectionLost = onConnectionLost;
+        client.onMessageArrived = onMessageArrived;
+
+        // Connect the client, providing an onConnect callback
+        client.connect({
+            onSuccess: onConnect,
+            onFailure: onFailure,
+            // userName: "fabian",
+            // password: "1234"
+        });
     }
 
     function onConnect() {
         console.log('Connected');
-        client.subscribe('prod/quantity');
+        client.subscribe("prod/quantity");
     }
 
     function onFailure(error) {
-        console.error('Failed to connect to MQTT broker:', error.message);
+        console.error('Failed to connect to MQTT broker:', error.errorMessage);
+        console.log(error);
         // Implement your own logic for handling connection failure, e.g., retry after a certain interval
         setTimeout(connectMQTT, 5000);
     }
 
-    function onConnectionLost() {
-        console.log('Connection Lost');
-        // Implement your own logic for handling connection loss, e.g., retry after a certain interval
-        setTimeout(connectMQTT, 5000);
+    function onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            console.log("Connection Lost: " + responseObject.errorMessage);
+            // Implement your own logic for handling connection loss, e.g., retry after a certain interval
+            setTimeout(connectMQTT, 5000);
+        }
     }
 
-    function onMessageArrived(topic, message) {
+    function onMessageArrived(data) {
         // update chart
-        const payload = JSON.parse(message.toString());
-        let line = payload[0].line;
-        let items = payload;
+        let line = JSON.parse(data.payloadString)[0].line;
+        let items = JSON.parse(data.payloadString);
         console.log(items);
         items.forEach(function(item) {
             if (item.line === line) {
