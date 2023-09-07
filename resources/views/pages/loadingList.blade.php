7eda708d16a08efefd711b432b134e3767196823
AIIA\fabian <fabian@aiia.co.id>
2023-09-07 08:18:10 +0700

    update table loading list

--- resources/views/pages/loadingList.blade.php
+++ resources/views/pages/loadingList.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.root.main')
 
 @section('main')
+    <div class="row">
+        <div class="col-md-12">
+            <div class=" card" style="padding: 2rem; border-radius:8px">
+                <input id="code" type="text" class="form-control" name="code" tabindex="1"
+                    placeholder="scan or type part here..." required autofocus autocomplete="off">
+            </div>
+        </div>
+    </div>
     <div class="card card-info mt-3 shadow" style="border-radius:10px">
         <div class="card-body">
             <h4 class="card-title mt-3 mb-3 text-dark text-center">DELIVERY MONITORING</h4>
