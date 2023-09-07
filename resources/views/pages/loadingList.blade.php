cc1f221b482c53256eb18105307b1434e8878cb8
AIIA\fabian <fabian@aiia.co.id>
2023-09-07 08:13:48 +0700

    update table loading list

--- resources/views/pages/loadingList.blade.php
+++ resources/views/pages/loadingList.blade.php
@@ -99,7 +99,7 @@
                                 </a>
                                 @if ($actualKanban >= $totalKanban)
                                     <button class="btn btn-outline-success">
-                                        <i class="fas fa-solid fa-check" style="margin-right: 0.8px"></i>
+                                        <i class="fas fa-solid fa-check" style="margin-right: 1px"></i>
                                         COMPLETE
                                     </button>
                                 @elseif ($actualKanban < $totalKanban && $actualKanban > 0)
