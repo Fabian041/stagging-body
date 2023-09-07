11955c5c16ff82ea25cd734b183652f3b46761b9
AIIA\fabian <fabian@aiia.co.id>
2023-09-07 08:11:47 +0700

    update table loading list

--- resources/views/pages/loadingList.blade.php
+++ resources/views/pages/loadingList.blade.php
@@ -104,7 +104,7 @@
                                     </button>
                                 @elseif ($actualKanban < $totalKanban && $actualKanban > 0)
                                     <button class="btn btn-outline-warning">
-                                        PROGRESS
+                                        ON PROGRESS
                                     </button>
                                 @elseif ($actualKanban == 0)
                                     <button class="btn btn-outline-danger">
