c4277074ad3e54392141d9f61774ff40ce59c80b
AIIA\fabian <fabian@aiia.co.id>
2023-09-12 15:40:12 +0700

    fix bug loading list detail

--- resources/views/pages/pulling/index.blade.php
+++ resources/views/pages/pulling/index.blade.php
@@ -1566,7 +1566,10 @@
                                             url: "{{ url('/loading-list/storeDetail') }}" +
                                                 '/' + ll + '/' + item
                                                 .part_number_cust +
-                                                '/' + item
+                                                '/' +
+                                                item
+                                                .part_number_int + '/' +
+                                                item
                                                 .total_kanban_qty +
                                                 '/' +
                                                 qty_per_kbn + '/' + item
