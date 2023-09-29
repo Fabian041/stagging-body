b8c4a87a2daef4361365dcc85ce446961cdc693d
AIIA\fabian <fabian@aiia.co.id>
2023-09-29 14:25:28 +0700

    mmki case

--- resources/views/pages/pulling/index.blade.php
+++ resources/views/pages/pulling/index.blade.php
@@ -1317,6 +1317,9 @@
                 barcodecomplete = barcode;
                 barcode = "";
 
+                console.log(barcodecomplete);
+                console.log(barcodecomplete.length);
+
                 if (barcodecomplete.charAt(0) == 'C') {
                     let loadingList = getLoadingListNumber();
                     $.ajax({
