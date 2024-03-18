854b87af84a4429ee8ca1a36da763975b642f974
AIIA\fabian <fabian@aiia.co.id>
2024-03-18 11:51:27 +0700

    fullscreen

--- resources/views/pages/production/index.blade.php
+++ resources/views/pages/production/index.blade.php
@@ -312,36 +312,21 @@
         initApp();
 
         document.getElementById('fullscreenBtn').addEventListener('click', function() {
-            if (!document.fullscreenElement) {
-                // Request fullscreen
-                if (document.documentElement.requestFullscreen) {
-                    document.documentElement.requestFullscreen();
-                } else if (document.documentElement.mozRequestFullScreen) {
-                    /* Firefox */
-                    document.documentElement.mozRequestFullScreen();
-                } else if (document.documentElement.webkitRequestFullscreen) {
-                    /* Chrome, Safari & Opera */
-                    document.documentElement.webkitRequestFullscreen();
-                } else if (document.documentElement.msRequestFullscreen) {
-                    /* IE/Edge */
-                    document.documentElement.msRequestFullscreen();
-                }
-            } else {
-                // Exit fullscreen
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.mozCancelFullScreen) {
-                    /* Firefox */
-                    document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) {
-                    /* Chrome, Safari and Opera */
-                    document.webkitExitFullscreen();
-                } else if (document.msExitFullscreen) {
-                    /* IE/Edge */
-                    document.msExitFullscreen();
-                }
-            }
+            localStorage.setItem('autoFullscreen', 'true');
+            enterFullscreen();
         });
+
+        function enterFullscreen() {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            }
+        }
+
+        if (localStorage.getItem('autoFullscreen') === 'true') {
+            document.addEventListener('DOMContentLoaded', (event) => {
+                enterFullscreen();
+            });
+        }
 
         $(document).on('click', function() {
             $('#code').focus();
@@ -386,7 +371,6 @@
         $('#release').on('click', function() {
             $('#code').focus();
             localStorage.clear();
-            localStorage.setItem('attemptFullscreen', 'true');
             window.location.reload();
         });
 
