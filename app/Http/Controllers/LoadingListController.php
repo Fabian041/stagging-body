0a1f1a1ab2a464fd1d1fbdd0720913c2ffc5c53f
AIIA\fabian <fabian@aiia.co.id>
2024-05-13 08:39:34 +0700

    rollback

--- app/Http/Controllers/LoadingListController.php
+++ app/Http/Controllers/LoadingListController.php
@@ -47,7 +47,7 @@ class LoadingListController extends Controller
 
     public function getLoadingList()
     {
-        $input = LoadingList::with('detail')->latest()->take(1000)->get();
+        $input = LoadingList::with('detail')->latest()->take(500)->get();
 
         return DataTables::of($input)
                 ->addColumn('customer', function ($loadingList) {
