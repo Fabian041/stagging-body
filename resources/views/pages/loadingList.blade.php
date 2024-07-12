f93d422c3d885ad144ec0341dfa7ec967ca10479
AIIA\fabian <mitsalf1@gmail.com>
2024-07-12 10:16:19 +0700

    optimize

--- resources/views/pages/loadingList.blade.php
+++ resources/views/pages/loadingList.blade.php
@@ -80,7 +80,7 @@
     $(document).ready(function() {
         let table = $('#loadingList').DataTable({
             scrollX: false,
-            processing: true,
+            processing: false,
             serverSide: true,
             ajax: {
                 url: `{{ url('dashboard/getLoadingList') }}`,
