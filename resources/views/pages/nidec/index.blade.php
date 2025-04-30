5e81d5a3975a2aa231fb832e5789291453f019fe
AIIA\fabian <mitsalf1@gmail.com>
2025-04-30 10:40:34 +0700

    debug

--- resources/views/pages/nidec/index.blade.php
+++ resources/views/pages/nidec/index.blade.php
@@ -418,7 +418,6 @@
                 barcodecomplete = barcode;
                 barcode = "";
 
-                alert(barcodecomplete.length);
                 if (barcodecomplete.length === 6) {
                     if (barcodecomplete == '000448' || barcodecomplete == '002484' || barcodecomplete ==
                         '000040' || barcodecomplete == '000504') {
