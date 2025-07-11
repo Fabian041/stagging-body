3ec82fa9281da700b4a17aeaeef91071c0a0d2c2
AIIA\fabian <mitsalf1@gmail.com>
2025-07-11 10:11:38 +0700

    grouping menu

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -7,30 +7,47 @@
             <img src="{{ asset('assets/img/bella.png') }}" alt="Bella" class="img-fluid mt-3" style="max-width: 50px;">
         </div>
         <ul class="sidebar-menu mt-2">
-            <li class="menu-header">Dashboard</li>
-            <li class="{{ request()->is('dashboard.receiving') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('dashboard.receiving') }}">
-                    <i class="fas fa-truck"></i>
-                    <span class="beep">Receiving</span>
-                </a>
+            <li class="menu-header">Main Menu</li>
+
+            {{-- PPIC Submenu --}}
+            <li
+                class="nav-item dropdown {{ request()->is('dashboard.receiving') || request()->is('loading-list') || request()->is('loading-list/*') ? 'active' : '' }}">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-warehouse"></i> <span>PPIC</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ request()->is('dashboard.receiving') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('dashboard.receiving') }}">
+                            <i class="fas fa-inbox"></i>
+                            <span>Receiving</span>
+                        </a>
+                    </li>
+                    <li class="{{ request()->is('loading-list') || request()->is('loading-list/*') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('loadingList.index') }}">
+                            <i class="fas fa-truck-loading"></i>
+                            <span>Delivery</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="{{ request()->is('dashboard') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('dashboard.index') }}">
-                    <i class="fas fa-fire"></i>
-                    <span class="beep">Dashboard</span>
-                </a>
-            </li>
-            <li class="{{ request()->is('dashboard/production/result') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('dashboard.prodResult') }}">
-                    <i class="fas fa-fire"></i>
-                    <span class="beep">Production Result</span>
-                </a>
-            </li>
-            <li class="{{ request()->is('loading-list') || request()->is('loading-list/*') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('loadingList.index') }}">
-                    <i class="fas fa-solid fa-list-ul"></i>
-                    <span class="">Loading List Details</span>
-                </a>
+
+            {{-- Production Submenu --}}
+            <li
+                class="nav-item dropdown {{ request()->is('dashboard') || request()->is('dashboard/production/result') ? 'active' : '' }}">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-industry"></i>
+                    <span>Production</span></a>
+                <ul class="dropdown-menu">
+                    <li class="{{ request()->is('dashboard') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('dashboard.index') }}">
+                            <i class="fas fa-box"></i>
+                            <span>Production Stock</span>
+                        </a>
+                    </li>
+                    <li class="{{ request()->is('dashboard/production/result') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('dashboard.prodResult') }}">
+                            <i class="fas fa-chart-line"></i>
+                            <span>Production Result</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </aside>
