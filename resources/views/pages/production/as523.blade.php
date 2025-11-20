c4f22bce0e761db457141e50e417cdd6266bf32d
AIIA\fabian <mitsalf1@gmail.com>
2025-11-20 11:08:45 +0700

    feat: fix len

--- resources/views/pages/production/as523.blade.php
+++ resources/views/pages/production/as523.blade.php
@@ -851,7 +851,7 @@
                         return;
                     }
 
-                    notif('success', LS.get('dandori_board') == k.internal);
+                    notif('success', LS.get('dandori_board'));
                     return;
 
                     if (LS.get('model') && LS.get('dandori_board')) {
