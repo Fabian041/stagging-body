d528dacb4b0200b86e99e6ee385b2b8276043c0d
Mochi8505 <imamfaturohman303@gmail.com>
2026-05-12 07:48:03 +0700

    fix: update part dock selection to use correct label for TAM-SPD

--- resources/views/pis/UpdatePis.blade.php
+++ resources/views/pis/UpdatePis.blade.php
@@ -405,8 +405,8 @@
                                         {{ isset($part_pis->part_dock) && $part_pis->part_dock == '6I' ? 'selected' : '' }}>
                                         6I</option>
                                     <option value="TAM-TAM"
-                                        {{ isset($part_pis->part_dock) && in_array($part_pis->part_dock, ['TAM-TAM', 'TAMTAM']) ? 'selected' : '' }}>
-                                        TAM-TAM</option>
+                                        {{ isset($part_pis->part_dock) && in_array($part_pis->part_dock, ['TAM-SPD', 'TAMSPD']) ? 'selected' : '' }}>
+                                        TAM-SPD</option>
                                     <option value="TAM-ADM"
                                         {{ isset($part_pis->part_dock) && in_array($part_pis->part_dock, ['TAM-ADM', 'TAMADM']) ? 'selected' : '' }}>
                                         TAM-ADM</option>
